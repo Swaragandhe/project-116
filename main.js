3b16ddf9c81757ddc59eaa0b4ae3f7dd8fe6174a
Swaragandhe <82380185+Swaragandhe@users.noreply.github.com>
2022-04-11 15:55:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+noseX = 0;
+noseY = 0;
+
+function preload() {
+lip = loadImage('https://i.postimg.cc/HstNvDzh/l1.png')
+}
+
+function setup() {
+canvas= createCanvas(300 , 300);
+canvas.center();
+video = createCapture(VIDEO);
+video.size(300 , 300);
+video.hide();
+poseNet = ml5.poseNet(video , modelLoaded);
+poseNet.on('pose' , gotPoses);
+}
+
+function gotPoses(results) {
+if(results.length>0){
+ noseX = results[0].pose.nose.x-10;
+ noseY = results[0].pose.nose.y+15;
+  console.log("nose x = " +  noseX );
+  console.log("nose y = " + noseY );
+}
+}
+
+function modelLoaded(){
+console.log('poseNet is initialized');
+}
+
+function draw() {
+image(video, 0 , 0 , 300 , 300);
+image(lip , noseX , noseY , 30 , 30);
+}
+
+function take_snapshot() {
+save('filter.png');
+}
